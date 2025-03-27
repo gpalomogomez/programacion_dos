caf187a5a6e9b8d8e9226e833ace99d9628b807e
Gsaad96 <gsaad@uade.edu.ar>
2025-03-27 15:21:08 -0300

    Fix Ejercicio 3

--- TP1/src/ejercicio3.java
+++ TP1/src/ejercicio3.java
@@ -3,15 +3,13 @@ import java.util.Scanner;
 public class ejercicio3 {
     public static void main(String[] args) {
 
-       // Scanner consola = new Scanner(System.in);
-       // int numero = consola.nextInt();
+        Scanner consola = new Scanner(System.in);
 
+        System.out.println("Ingrese 1 numero");
+        int numero = consola.nextInt();
 
         for(int i=1; i<11; i++) {
-            for(int j=1;j<11;j++)
-            {
-                System.out.println("Tabla: "+ i +"=" + i * j);
-            }
+            System.out.println("Tabla: "+ i + "*" + numero +" = " + i * numero);
         }
     }
 }
