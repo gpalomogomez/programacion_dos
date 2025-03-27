48d25a46912932b2600229ec89b45d1da2b4fa04
Gsaad96 <gsaad@uade.edu.ar>
2025-03-27 13:37:17 -0300

    Ejercicios 2, 3, 4 y 8

---
+++ TP1/src/ejercicio2.java
@@ -0,0 +1,22 @@
+package tp1;
+
+import java.util.Scanner;
+
+public class ejercicio2 {
+    public static void main(String[] args) {
+
+        boolean esPrimo = true;
+        Scanner input = new Scanner(System.in);
+
+        int numero = input.nextInt();
+
+        for(int i = 2; i<numero-1;i++){
+            if(numero%i==0){
+                esPrimo = false;
+                break;
+            }
+        }
+
+        System.out.println("El numero: "+ numero +" " + ((esPrimo) ? "Es primo" : "No es primo"));
+    }
+}
---
+++ TP1/src/ejercicio4.java
@@ -0,0 +1,22 @@
+package tp1;
+
+import java.util.Scanner;
+
+public class ejercicio4 {
+    public static void main(String[] args) {
+
+        int contador = 0;
+        int resto = 0;
+        double resultado;
+        Scanner input = new Scanner(System.in);
+
+        int numero = input.nextInt();
+        do{
+            numero = numero/10;
+            contador++;
+
+        }
+        while(numero>0);
+        System.out.println(contador);
+    }
+}
