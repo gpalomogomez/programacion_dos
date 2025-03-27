4986ab03fe5e86435a5b7be088281f68b0eeec3d
Gsaad96 <gsaad@uade.edu.ar>
2025-03-27 14:29:48 -0300

    Ejercicio 1

--- TP1/src/ejercicio8.java
+++ TP1/src/ejercicio8.java
@@ -1,5 +1,3 @@
-package tp1;
-
 import java.util.Scanner;
 
 public class ejercicio8 {
@@ -28,7 +26,11 @@ public class ejercicio8 {
                     saldo = setSaldo(saldo,input);
                     break;
                 case 3 :
+                    saldo = removeSaldo(saldo,input);
+                    break;
+                case 4 :
                     salir = false;
+                    break;
                 default:
                     System.out.println("error");
             }
@@ -41,6 +43,10 @@ public class ejercicio8 {
 
     static double setSaldo(Double saldo,Scanner input) {
         return input.nextDouble() + saldo;
+    }
+
+    static double removeSaldo(Double saldo,Scanner input) {
+        return saldo - input.nextDouble();
     }
 
 
