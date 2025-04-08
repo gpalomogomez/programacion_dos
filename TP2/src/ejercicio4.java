c0dfba3e4da79e41585b53afc11a030da1f30733
gpalomogomez <gpalomogomez@uade.edu.ar>
2025-04-08 18:08:36 -0300

    ejercio 4 y 5 terminado

--- TP2/src/ejercicio4.java
+++ TP2/src/ejercicio4.java
@@ -1,5 +1,54 @@
+import java.util.Random;
+
 public class ejercicio4 {
     public static void main(String[] args) {
 
+        int semanas = 4;
+        int dias = 7;
+        double[][] temperaturas = new double[4][7];
+        Random random = new Random();
+
+        double[] temperaturaMediaSemanal = new double[semanas];
+        double temperaturaMediaMensual = 0;
+
+        double maxTemp = Double.MIN_VALUE;
+        double minTemp = Double.MAX_VALUE;
+        int semanaMax = 0;
+        int diaMax = 0;
+        int semanaMin = 0;
+        int diaMin = 0;
+
+        for (int i = 0; i < semanas; i++) {
+            for (int j = 0; j < dias; j++) {
+                temperaturas[i][j] = random.nextDouble(100);
+                temperaturaMediaSemanal[i] += temperaturas[i][j];
+                temperaturaMediaMensual += temperaturas[i][j];
+
+               if (temperaturas[i][j] > maxTemp) {
+                    maxTemp = temperaturas[i][j];
+                    semanaMax = i;
+                    diaMax = j;
+                }
+
+                if (temperaturas[i][j] < minTemp) {
+                    minTemp = temperaturas[i][j];
+                    semanaMin = i;
+                    diaMin = j;
+                }
+            }
+        }
+
+        for (int i = 0; i < semanas; i++) {
+            System.out.println("Temperatura media semana "+(i+1)+" : " + Math.round(temperaturaMediaSemanal[i]/dias));
+        }
+
+        System.out.println("Temperatura media mensual : "  + Math.round(temperaturaMediaMensual/(semanas*dias)));
+
+        System.out.println("Fecha mas caluroso : semana "+(semanaMax+1)+" día "+(diaMax+1) + " con " + maxTemp + " grados");
+
+        System.out.println("Fecha más frio : semana "+(semanaMin+1)+" día "+(diaMin+1) + " con " + minTemp + " grados");
+
     }
+
+
 }
--- TP2/src/ejercicio5.java
+++ TP2/src/ejercicio5.java
@@ -1,5 +1,58 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class ejercicio5 {
     public static void main(String[] args) {
+        Random random = new Random();
+        int n = random.nextInt(1,10);
+        int[][] A = aleatorioInt(n);
+        int[][] B = aleatorioInt(n);
+        int[][] resultadoSuma = sumarMatriz(A,B,"sumar");
+        int[][] resultadoResta = sumarMatriz(A,B,"restar");
 
+        imprimirMatriz(A);
+        System.out.println("Separando");
+        imprimirMatriz(B);
+        System.out.println("Resultado Suma: ");
+        imprimirMatriz(resultadoSuma);
+        System.out.println("Resultado Resta: ");
+        imprimirMatriz(resultadoResta);
+
+    }
+
+
+    public static int[][] aleatorioInt(int n) {
+        Random random = new Random();
+        int[][] matriz = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matriz[i][j] = random.nextInt(15);
+            }
+        }
+        return matriz;
+    }
+
+    public static void imprimirMatriz(int[][] matriz) {
+        for (int[] fila : matriz) {
+            for (int valor : fila) {
+                System.out.printf("%3d ", valor);
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] sumarMatriz(int[][] A, int[][] B,String operacion) {
+        int[][] resultado = new int[A.length][A.length];
+        for(int i = 0; i < A.length; i++) {
+            for(int j = 0; j < A.length; j++) {
+                if("restar".equals(operacion)) {
+                    resultado[i][j] = A[i][j] - B[i][j];
+                }
+                else {
+                    resultado[i][j] = A[i][j] + B[i][j];
+                }
+            }
+        }
+        return resultado;
     }
 }
